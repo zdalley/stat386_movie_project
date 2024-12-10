85ee511d42986f8f9f48964737495d154c63c4d9
Quinn Wright <153124517+quinny7777@users.noreply.github.com>
2024-12-09 21:03:14 -0700

    Added Scatterplots

--- streamlitapp.py
+++ streamlitapp.py
@@ -21,3 +21,30 @@ ax.set_title(f"Histogram of {hist_column}")
 ax.set_xlabel(hist_column)
 ax.set_ylabel("Frequency")
 st.pyplot(fig)
+st.header("Scatterplots")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    fig1, ax1 = plt.subplots()
+    ax1.scatter(frame2["budget"], frame2["rev_worldwide"], alpha=0.7, color="blue")
+    ax1.set_title(f"Budget vs Worldwide Revenue")
+    ax1.set_xlabel("Budget")
+    ax1.set_ylabel("Worldwide Revenue")
+    st.pyplot(fig1)
+with col2:
+    fig2, ax2 = plt.subplots()
+    ax2.scatter(frame2["rev_opening_weekend_NA"], frame2["rev_NA"], alpha=0.7, color="blue")
+    ax2.set_title(f"Opening Weekend vs Total Revenue (North America)")
+    ax2.set_xlabel("Opening Weekend Revenue (North America)")
+    ax2.set_ylabel("Total Revenue (North America)")
+    st.pyplot(fig2)
+with col3:
+    fig3, ax3 = plt.subplots()
+    ax3.scatter(frame2["user_rating"], frame2["critic_rating"], alpha=0.7, color="blue")
+    ax3.set_title(f"User vs Critic Rating")
+    ax3.set_xlabel("User Rating")
+    ax3.set_ylabel("Critic Rating")
+    st.pyplot(fig3)
+
+
