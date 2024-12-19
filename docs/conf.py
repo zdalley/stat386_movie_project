6e9e05dc9a06d203b0cfd0135fbf56146464ed37
zdalley <zackdalley45@gmail.com>
2024-12-18 19:37:33 -0700

    Fixed documentation

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Movie Package'
+copyright = '2024, Zack Dalley, Quinn Wright, Colby Eastmond'
+author = 'Zack Dalley, Quinn Wright, Colby Eastmond'
+release = '0.0.1'
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode',
+              'myst_parser', 'sphinx.ext.githubpages']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+source_suffix = ['.rst', '.md']
